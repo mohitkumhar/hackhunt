43c72819149ebddb3d09cdd68aa1dad8bc2fc2e0
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-04-07 15:46:30 +0530

    Added the blind coding platform

--- packages/socket/src/services/game.ts
+++ packages/socket/src/services/game.ts
@@ -1,5 +1,4 @@
-/* eslint-disable max-lines, max-params */
-import { Answer, GameMode, Player, Quizz, ReverseQuizz } from "@rahoot/common/types/game"
+import { Answer, BlindCodingQuizz, GameMode, Player, Quizz, ReverseQuizz } from "@rahoot/common/types/game"
 import { Server, Socket } from "@rahoot/common/types/game/socket"
 import { Status, STATUS, StatusDataMap } from "@rahoot/common/types/game/status"
 import { usernameValidator } from "@rahoot/common/validators/auth"
